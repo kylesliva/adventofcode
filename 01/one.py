9820f8cd4b5fa5179f6e943bab669d10ee4ca41c
Kyle Sliva <kyle@kylesliva.me>
2022-03-14 14:13:09 -0400

    set default log level to INFO

--- 01/one.py
+++ 01/one.py
@@ -5,7 +5,7 @@ import logging
 import pprint as p
 import pytest
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 '''
 todo
