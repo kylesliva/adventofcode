4b19a4b87936e574f2b23a42539166604b374b76
Kyle Sliva <kyle@kylesliva.me>
2022-03-14 14:12:47 -0400

    add cli arg capability

--- 01/one.go
+++ 01/one.go
@@ -1,28 +1,59 @@
+// run with go run one.go </path/to/input-file>
 package main
 
 import(
 	"fmt"
-	// "os"
+	"io/ioutil"
+	"bufio"
+	"bytes"
+	"strconv"
+	"os"	
 )
 
-func main(){
-	// create slice of test masses
-	masses := []float32{14, 1969, 100756}
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
-	// fmt.Println(masses)
+func main(){
+
+	// pull input file path from CLI
+	fileloc := os.Args[1]
+
+	dat, err := ioutil.ReadFile(fileloc)
+	check(err)
+
+	var masses []float32
+
+	scanner := bufio.NewScanner(bytes.NewReader(dat))
+	for scanner.Scan() {
+		value, err := strconv.ParseFloat(scanner.Text(), 32)
+		check(err)
+
+		masses = append(masses, float32(value))
+	}
 
 	var fuel []float32 
 
 	for _, v := range masses {
-		// fmt.Println(v)
 		var computedFuel float32
 		computedFuel = computeFuel(v)
 		fuel = append(fuel, computedFuel)
 	}
 
-	fmt.Println(fuel)
+	fmt.Println(int(sum(fuel)))
 
 }
+
+func sum(array []float32) float32 {  
+	var result float32
+	result = 0  
+	for _, v := range array {  
+	 result += v  
+	}  
+	return result  
+   }  
 
 // func readMass(path) {
 
